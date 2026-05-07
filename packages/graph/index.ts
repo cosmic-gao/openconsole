745043c5d5ff9a802e469631c6e5aea780ad767b
August.Gao <August.Gao@mspbots.ai>
2026-05-07 12:16:33 +0800

    refactor(graph): 重构端口和边类型以增强类型安全
    - 将 Port 类改为抽象类，Input/Output 继承并实现 direction 属性
    - 为 Socket 添加 isCompatibleWith 方法用于兼容性检查
    - 重构 Edge 类，使用 Endpoint 类型替代简单的键名，提高类型准确性
    - 添加类型别名 Sockets、Inputs、Outputs 以简化泛型参数
    - 统一字符串字面量使用单引号格式

--- packages/graph/index.ts
+++ packages/graph/index.ts
@@ -12,59 +12,60 @@ export type SocketId = Id<'Socket'>
 export type PortId = Id<'Port'>
 export type EndpointId = Id<'Endpoint'>
 
-export type Direction = "input" | "output"
+export type Direction = 'input' | 'output'
 
 export class Socket {
   public constructor(public readonly name: string) { }
+
+  public isCompatibleWith(other: Socket): boolean {
+    return this === other || this.name === other.name
+  }
 }
 
-export class Port<S extends Socket> {
+export abstract class Port<S extends Socket = Socket> {
+  public abstract readonly direction: Direction
+
   public constructor(
     public readonly socket: S,
-    public readonly index?: number,
-    public readonly id?: PortId,
-  ) {
-  }
+    public readonly id: PortId,
+    public readonly label?: string,
+  ) { }
 }
 
-export class Input<S extends Socket> extends Port<S> {
-  public static direction: Direction = 'input'
-
-  public constructor(public socket: S) {
-    super(socket)
-  }
+export class Input<S extends Socket = Socket> extends Port<S> {
+  public readonly direction = 'input' as const
 }
 
-export class Output<S extends Socket> extends Port<S> {
-  public static direction: Direction = 'output'
-
-  public constructor(public socket: S) {
-    super(socket)
-  }
+export class Output<S extends Socket = Socket> extends Port<S> {
+  public readonly direction = 'output' as const
 }
+
+export type Sockets = { [K in string]?: Socket }
+export type Inputs<I extends Sockets> = { [K in keyof I]?: Input<NonNullable<I[K]>> }
+export type Outputs<O extends Sockets> = { [K in keyof O]?: Output<NonNullable<O[K]>> }
 
 export class Node<
-  I extends { [K in string]?: Socket } = { [K in string]?: Socket },
-  O extends { [K in string]?: Socket } = { [K in string]?: Socket },
+  I extends Sockets = Sockets,
+  O extends Sockets = Sockets,
 > {
-  public readonly inputs: { [K in keyof I]?: Input<Exclude<I[K], undefined>> } = {}
-  public readonly outputs: { [K in keyof O]?: Output<Exclude<O[K], undefined>> } = {}
+  public readonly inputs: Inputs<I> = {}
+  public readonly outputs: Outputs<O> = {}
 
   public constructor(public readonly id: NodeId) { }
 }
 
-export interface Endpoint<N extends Node, P extends Port<Socket>> {
-  node: N
-  port: P
+export interface Endpoint<N extends Node, P extends Port> {
+  readonly node: N
+  readonly port: P
 }
 
 export class Edge<
-  S extends Node,
-  T extends Node
+  S extends Node = Node,
+  T extends Node = Node,
 > {
   public constructor(
-    public readonly source: keyof S['inputs'],
-    public readonly target: keyof T['outputs'],
+    public readonly source: Endpoint<S, NonNullable<S['outputs'][keyof S['outputs']]>>,
+    public readonly target: Endpoint<T, NonNullable<T['inputs'][keyof T['inputs']]>>,
     public readonly id: EdgeId,
   ) { }
 }
